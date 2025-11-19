6ce83f5ba1253193a0c5c56e52a035b0198b04b6
e60105023-cell <e60105023@gmail.com>
2025-11-19 14:09:59 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+from aiogram import Bot, Dispatcher, Router
+from aiogram.types import Message
+from aiogram.filters import CommandStart
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")  # берём токен из переменной окружения
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+router = Router()
+
+@router.message(CommandStart())
+async def start_handler(message: Message):
+    await message.answer("Привет! Я эхо-бот.")
+
+@router.message()
+async def echo_handler(message: Message):
+    await message.answer(message.text)
+
+dp.include_router(router)
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(main())
